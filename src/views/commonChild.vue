c32e7e162e61931b644d26c7614f82293d5a7dbc
zy <zy@163.comm>
2023-08-06 19:42:06 +0800

    a

---
+++ src/views/commonChild.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        子组件
+        {{ msg }}
+        {{ a }}
+        <ul>
+            <li v-for="item in arr">
+                {{ item }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            msg: String,
+            a:{
+                type: String,
+                default: '我是默认数据'
+            },
+            arr: {
+                type: Array,
+                default: [4,5,6]
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/commonHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>
+            父组件
+        </h1>
+        <br>
+        <commonChild :msg="msg" :obj="obj" :arr="arr"/>
+    </div>
+</template>
+
+<script>
+import commonChild from '@/views/commonChild.vue'
+    export default {
+        data () {
+            return {
+                msg: '我是子组件的数据',
+                obj: {
+                    a: 1,
+                    b: 2
+                },
+                arr:[1,2,3]
+            }
+        },
+        components: {
+            commonChild
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
